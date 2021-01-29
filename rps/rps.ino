b4fc33a29d35b3b72bd201edd1f6957dca78aa31
Theo Butler <theodusbutler@gmail.com>
2021-01-29 12:35:59 -0500

    WIP: publish to topic

--- rps/rps.ino
+++ rps/rps.ino
@@ -2,6 +2,12 @@
 #include "rps_sensor.h"
 
 #include <Arduino.h>
+#include <ros.h>
+#include <std_msgs/Float32.h>
+
+ros::NodeHandle node;
+std_msgs::Float32 mcu_rps_msg;
+ros::Publisher mcu_rps("mcu/rps", &mcu_rps_msg);
 
 RPSSensors rps(A0, A8);
 
@@ -12,7 +18,14 @@ void setup()
 
 void loop()
 {
+  node.spinOnce();
   bool velocity_update = rps.update();
   if (!velocity_update)
     return;
+
+  Array<float, 2> rps_values = rps.rps();
+  mcu_rps_msg.data = rps_values[0];
+  node.publish(&mcu_rps_msg);
+  mcu_rps_msg.data = rps_values[1];
+  node.publish(&mcu_rps_msg);
 }
