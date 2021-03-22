d7c9243b6ce90781a2abfb3aa88efb3549a92678
theodus <theodusbutler@gmail.com>
2021-03-22 18:58:48 -0400

    refactor ROS node

---
+++ mcu/imu.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <ICM_20948.h>
+#include <SPI.h>
+
+class IMU
+{
+  ICM_20948_SPI device;
+
+public:
+  IMU(uint8_t cs_pin)
+  {
+    SPI.begin();
+    device.begin(cs_pin, SPI);
+  }
+
+  inline ICM_20948_Status_e status() const
+  {
+    return device.status;
+  }
+
+  inline char const* status_str()
+  {
+    return device.statusString();
+  }
+};
--- mcu/mcu.ino
+++ mcu/mcu.ino
@@ -1,49 +1,27 @@
+#include "imu.h"
+#include "ros_node.h"
 #include "rps_sensors.h"
 
 #include <Arduino.h>
-#include <ArduinoHardware.h>
 #include <Servo.h>
-#include <ros.h>
-#include <ros/node_handle.h>
-#include <std_msgs/String.h>
-#include <std_msgs/UInt8.h>
-
-// template arguments: MAX_SUBSCRIBERS, MAX_PUBLISHERS, INPUT_SIZE, OUTPUT_SIZE
-ros::NodeHandle_<ArduinoHardware, 1, 1, 128, 128> node;
-
-std_msgs::String mcu_dbg_msg;
-ros::Publisher mcu_dbg("mcu/dbg", &mcu_dbg_msg);
-
-void mcu_ctl_cb(std_msgs::UInt8 const&);
-ros::Subscriber<std_msgs::UInt8> mcu_ctl("mcu/ctl", &mcu_ctl_cb);
 
 RPSSensors<2> rps({A0, A8});
-
+IMU imu(2);
 Servo throttle;
 
 static int16_t throttle_setpoint = 0.0;
-void mcu_ctl_cb(std_msgs::UInt8 const& msg)
-{
-  throttle_setpoint = (int16_t)msg.data;
-
-  static char buf[16];
-  sprintf(buf, "ctl: %x", msg.data);
-  mcu_dbg_msg.data = buf;
-  mcu_dbg.publish(&mcu_dbg_msg);
-}
+ROSNode node([](auto const& msg) { throttle_setpoint = (int16_t)msg.data; });
 
 void setup()
 {
-  node.initNode();
-  node.advertise(mcu_dbg);
-  node.subscribe(mcu_ctl);
-
   throttle.attach(8);
+  if (imu.status() != ICM_20948_Stat_Ok)
+    node.debug("%s", imu.status_str());
 }
 
 void loop()
 {
-  node.spinOnce();
+  node.spin_once();
 
   bool rps_update = rps.update();
   if (!rps_update)
@@ -52,15 +30,11 @@ void loop()
   Array<float, 2> rps_values = rps.values();
   float avg_rps = (rps_values[0] + rps_values[1]) / 2.0;
 
-  static char buf[32];
-  sprintf(
-    buf,
+  node.debug(
     "value: %d, %d.%d",
     throttle_setpoint,
     (int16_t)round(avg_rps),
     (int16_t)round((avg_rps - floor(avg_rps)) * 10));
-  mcu_dbg_msg.data = buf;
-  mcu_dbg.publish(&mcu_dbg_msg);
 
   static constexpr int16_t throttle_neutral = 90;
   throttle.write(throttle_neutral + throttle_setpoint);
