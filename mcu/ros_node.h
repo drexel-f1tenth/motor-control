7f892ebf5922c2b601ad6dc3bc957015fb154d7f
Theo Butler <theodusbutler@gmail.com>
2021-03-22 20:04:20 -0400

    use rosserial logging

--- mcu/ros_node.h
+++ mcu/ros_node.h
@@ -4,32 +4,36 @@
 #include <ArduinoHardware.h>
 #include <ros.h>
 #include <ros/node_handle.h>
-#include <std_msgs/String.h>
 #include <std_msgs/UInt8.h>
 
 class ROSNode
 {
 public:
-  static constexpr char const* debug_topic = "mcu/dbg";
   static constexpr char const* control_topic = "mcu/ctl";
 
+  using Log = rosserial_msgs::Log;
   using Msg = std_msgs::UInt8;
   using Callback = void (*)(Msg const&);
 
 private:
-  // template arguments: MAX_SUBSCRIBERS, MAX_PUBLISHERS, INPUT_SIZE,
-  // OUTPUT_SIZE
-  ros::NodeHandle_<ArduinoHardware, 1, 1, 128, 128> _node;
-  std_msgs::String _mcu_dbg_msg;
-  ros::Publisher _mcu_dbg{debug_topic, &_mcu_dbg_msg};
+  // Explicitly restrict memory utilization from NodeHandle.
+  // template args: MAX_SUBSCRIBERS, MAX_PUBLISHERS, INPUT_SIZE, OUTPUT_SIZE
+  ros::NodeHandle_<ArduinoHardware, 1, 0, 128, 128> _node;
   ros::Subscriber<std_msgs::UInt8> _mcu_ctl;
+  char _msg_buf[32];
 
 public:
-  ROSNode(Callback cb) : _mcu_ctl{control_topic, cb}
+  ROSNode(Callback cb) : _mcu_ctl{control_topic, cb} {}
+
+  inline void init()
   {
     _node.initNode();
-    _node.advertise(_mcu_dbg);
     _node.subscribe(_mcu_ctl);
+  }
+
+  inline bool connected()
+  {
+    return _node.connected();
   }
 
   inline void spin_once()
@@ -37,13 +41,15 @@ public:
     _node.spinOnce();
   }
 
-  inline void debug(char const* fmt, ...)
+  template<Log::_level_type level = Log::INFO>
+  inline void log(char const* fmt, ...)
   {
+    Log msg;
+    msg.level = level;
+    msg.msg = _msg_buf;
     va_list args;
     va_start(args, fmt);
-    static char msg_buf[32];
-    vsnprintf(msg_buf, sizeof(msg_buf), fmt, args);
-    _mcu_dbg_msg.data = msg_buf;
-    _mcu_dbg.publish(&_mcu_dbg_msg);
+    vsnprintf(_msg_buf, sizeof(_msg_buf), fmt, args);
+    _node.publish(rosserial_msgs::TopicInfo::ID_LOG, &msg);
   }
 };
