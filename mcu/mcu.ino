e268b850cd9e1cbe7a039033359e54158fb8021f
theodus <theodusbutler@gmail.com>
2021-04-28 16:31:49 -0400

    complex throttle control

--- mcu/mcu.ino
+++ mcu/mcu.ino
@@ -2,6 +2,7 @@
 #include "pid.hh"
 #include "ros_node.hh"
 #include "rps_sensor.hh"
+#include "throttle.hh"
 #include "timer_interrupt.hh"
 
 #include <Arduino.h>
@@ -10,18 +11,8 @@
 RPSSensor rps{A0, A1};
 IMU imu;
 Servo steering;
-Servo throttle;
+Throttle throttle;
 PID pid{1.8, 0.03, 0.0};
-
-static void set_steering(int16_t);
-static void set_throttle(int16_t);
-
-static int16_t throttle_setpoint = 0;
-ROSNode node{[](auto const& serialized) {
-  ROSNode::Ctl msg{serialized};
-  throttle_setpoint = (int16_t)msg.throttle;
-  set_steering(msg.steering);
-}};
 
 static inline void set_steering(int16_t setpoint)
 {
@@ -31,13 +22,12 @@ static inline void set_steering(int16_t setpoint)
   steering.write(steering_neutral + setpoint);
 }
 
-static inline void set_throttle(int16_t adjust)
-{
-  static constexpr int16_t throttle_neutral = 90;
-  static constexpr int16_t throttle_cap = 30;
-  adjust = constrain(adjust, -throttle_cap, throttle_cap);
-  throttle.write(throttle_neutral + adjust);
-}
+static int16_t throttle_setpoint = 0;
+ROSNode node{[](auto const& serialized) {
+  ROSNode::Ctl msg{serialized};
+  throttle_setpoint = (int16_t)msg.throttle;
+  set_steering(msg.steering);
+}};
 
 void setup()
 {
@@ -46,8 +36,7 @@ void setup()
   steering.attach(6);
   set_steering(0);
 
-  throttle.attach(8);
-  set_throttle(0);
+  throttle.init(8);
 
   node.init();
   imu.init();
@@ -78,7 +67,6 @@ void update_64hz()
   else
     adjust = throttle_setpoint;
 
-  static unsigned long prev_ms = 0;
   node.log(
     "RPS: %d %d %d.%u",
     throttle_setpoint,
@@ -86,7 +74,7 @@ void update_64hz()
     (int16_t)rps.value(),
     first_decimal(rps.value()));
 
-  set_throttle(adjust);
+  throttle.set_position(adjust);
 
   imu.update();
   if (imu.ready())
@@ -121,6 +109,7 @@ void loop()
 {
   node.spin_once();
 
+  // For most accuate results, unset timer_interrupt_flag as soon as it is read.
   bool const timer_fired = timer_interrupt_flag;
   if (timer_fired)
     timer_interrupt_flag = false;
---
+++ mcu/throttle.hh
@@ -0,0 +1,112 @@
+#pragma once
+
+#include "timer_interrupt.hh"
+
+#include <Arduino.h>
+#include <Servo.h>
+
+// TODO: document state machine
+class Throttle
+{
+  static constexpr int16_t esc_neutral = 90;
+  static constexpr int16_t throttle_cap = 40;
+  static constexpr int16_t break_magnitude = 90;
+
+  enum struct State : uint8_t
+  {
+    // clang-format off
+    Stop           = 0b0000,
+    Forward        = 0b0100,
+    ForwardNeutral = 0b0101,
+    ForwardBreak   = 0b0110,
+    Reverse        = 0b1000,
+    ReverseNeutral = 0b1001,
+    ReverseBreak   = 0b1010
+    // clang-format on
+  };
+
+  Servo _esc;
+  int16_t _position = 0;
+  State _state = State::Stop;
+
+public:
+  Throttle() {}
+
+  void init(uint8_t esc_pin)
+  {
+    _esc.attach(esc_pin);
+    write_position(0);
+  }
+
+  void update(bool wheels_stopped)
+  {
+    if (!wheels_stopped)
+      return;
+
+    // Transition from neutral or break to stop.
+    if ((uint8_t)_state & 0b0011)
+      _state = State::Stop;
+  }
+
+  void set_position(int16_t position)
+  {
+    _position = position;
+
+    if (position == 0)
+    {
+      write_position(0);
+      if (moving_forward())
+        _state = State::ForwardNeutral;
+      else if (moving_reverse())
+        _state = State::ReverseNeutral;
+    }
+    else if ((_state != State::Stop) && ((position > 0) != moving_forward()))
+    {
+      apply_break();
+    }
+    else if (position > 0)
+    {
+      write_position(position);
+      _state = State::Forward;
+    }
+    else if (position < 0)
+    {
+      write_position(position);
+      _state = State::Reverse;
+    }
+  }
+
+private:
+  inline bool moving_forward() const
+  {
+    return ((uint8_t)_state & 0b0100) != 0;
+  }
+
+  inline bool moving_reverse() const
+  {
+    return ((uint8_t)_state & 0b1000) != 0;
+  }
+
+  inline void write_position(int16_t position)
+  {
+    position = constrain(position, -throttle_cap, throttle_cap);
+    _esc.write(esc_neutral + position);
+  }
+
+  inline void apply_break()
+  {
+    if (_state == State::Stop)
+      return;
+
+    if (moving_forward())
+    {
+      _esc.write(-break_magnitude);
+      _state = State::ForwardBreak;
+    }
+    else if (moving_reverse())
+    {
+      _esc.write(break_magnitude);
+      _state = State::ReverseBreak;
+    }
+  }
+};
