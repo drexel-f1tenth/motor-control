804a93335b26df6e768e7a68acb399e5f3ec0e02
theodus <theodusbutler@gmail.com>
2021-04-28 12:56:33 -0400

    add some documentation to IMU

--- mcu/imu.hh
+++ mcu/imu.hh
@@ -5,11 +5,13 @@
 #include <ICM_20948.h>
 #include <Wire.h>
 
+/// Inertial Measurement Unit connection.
 class IMU
 {
   ICM_20948_I2C _device;
 
 public:
+  /// Setup the I2C connection to the device.
   inline void init()
   {
     Wire.begin();
@@ -17,35 +19,33 @@ public:
     _device.begin(Wire, 1);
   }
 
+  /// Return the device status.
+  inline ICM_20948_Status_e status() const
+  {
+    return _device.status;
+  }
+
+  /// Return the status string for the current status.
+  inline char const* status_str()
+  {
+    return _device.statusString();
+  }
+
+  /// Reset the device.
   inline ICM_20948_Status_e reset()
   {
     _device.swReset();
     return status();
   }
 
-
-  inline ICM_20948_Status_e wakeup()
-  {
-    _device.sleep(false);
-    _device.lowPower(false);
-    return status();
-  }
-
+  /// Return true if data from the device is ready. If true, use `update` to
+  /// update the values.
   inline bool ready()
   {
     return _device.dataReady();
   }
 
-  inline ICM_20948_Status_e status() const
-  {
-    return _device.status;
-  }
-
-  inline char const* status_str()
-  {
-    return _device.statusString();
-  }
-
+  /// Update the data from the device.
   inline void update()
   {
     _device.getAGMT();
@@ -66,4 +66,3 @@ public:
     return {_device.magX(), _device.magY(), _device.magZ()};
   }
 };
-
